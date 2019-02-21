fe01477c20756d30c277b2a3ad582ced41eeeeb2
Justin Clark <justin.d.clark@protonmail.com>
2019-02-21 14:07:32 -0500

    Added entity models

---
+++ model/Survey.java
@@ -0,0 +1,150 @@
+package com.revature.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+
+@Table(name = "surveys")
+@Entity
+public class Survey {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "survey_id")
+	private int surveyId;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User creator;
+	
+	private String description;
+	private Date creationDate;
+	private Date expirationDate;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "status_id")
+	private Status status;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "restriction_id")
+	private Restriction restriction;
+
+	public Survey() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the surveyId
+	 */
+	public int getSurveyId() {
+		return surveyId;
+	}
+
+	/**
+	 * @param surveyId the surveyId to set
+	 */
+	public void setSurveyId(int surveyId) {
+		this.surveyId = surveyId;
+	}
+
+	/**
+	 * @return the creator
+	 */
+	public User getCreator() {
+		return creator;
+	}
+
+	/**
+	 * @param creator the creator to set
+	 */
+	public void setCreator(User creator) {
+		this.creator = creator;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the creationDate
+	 */
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	/**
+	 * @param creationDate the creationDate to set
+	 */
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	/**
+	 * @return the expirationDate
+	 */
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+
+	/**
+	 * @param expirationDate the expirationDate to set
+	 */
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public Status getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the restriction
+	 */
+	public Restriction getRestriction() {
+		return restriction;
+	}
+
+	/**
+	 * @param restriction the restriction to set
+	 */
+	public void setRestriction(Restriction restriction) {
+		this.restriction = restriction;
+	}
+	
+	
+}
