e18ae0eaf285af264a1aa3a950cbbd3cb17ccb72
Aaron Gravelle <agrav12825@gmail.com>
2019-02-23 20:02:56 -0500

    add missing code to WebConfig.java

--- SurveySaysAPI/src/main/java/com/revature/configuration/WebConfig.java
+++ SurveySaysAPI/src/main/java/com/revature/configuration/WebConfig.java
@@ -1,14 +1,52 @@
 package com.revature.configuration;
 
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.revature.util.DeepFieldFilter;
 
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-	@Override
+
+	//More configuration....
+
+    /* Here we register the Hibernate4Module into an ObjectMapper, then set this custom-configured ObjectMapper
+     * to the MessageConverter and return it to be added to the HttpMessageConverters of our application*/
+    public MappingJackson2HttpMessageConverter jacksonMessageConverter(){
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+
+        ObjectMapper mapper = new ObjectMapper();
+        //Registering Hibernate5Module to support lazy objects
+//        mapper.registerModule(new Hibernate5Module());
+        SimpleFilterProvider depthFilters = new SimpleFilterProvider().addFilter("depth_1", new DeepFieldFilter(1))
+                .addFilter("depth_2", new DeepFieldFilter(2))
+                .addFilter("depth_3", new DeepFieldFilter(3))
+                .addFilter("depth_4", new DeepFieldFilter(4))
+                .addFilter("depth_5", new DeepFieldFilter(5));
+		mapper.setFilterProvider(depthFilters);
+
+        messageConverter.setObjectMapper(mapper);
+        return messageConverter;
+    }
+    
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //Here we add our custom-configured HttpMessageConverter
+        converters.add(jacksonMessageConverter());
+    }
+	
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // all endpoints in the api
             .allowedOrigins("http://localhost:9001", "http://localhost:3000") // allow this origin
@@ -31,5 +69,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/api/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
         registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
 }
